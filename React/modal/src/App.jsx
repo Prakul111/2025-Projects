b3724f46bc8c0c6d5e19d9a38f910d55e5e58448
@Prakul111 <iamprakul111@gmail.com>
2024-12-31 19:14:53 +0530

    01

---
+++ React/modal/src/App.jsx
@@ -0,0 +1,11 @@
+import "./user.css"
+import user from "./user.json"
+import { UserCard } from "./UserCard"
+
+export default function App() {
+  return (
+    <UserCard name={user.name} phoneNumber={user.phoneNumber} age={user.age} address={user.address} />
+  )
+}
+
+
