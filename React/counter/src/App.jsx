c17d2725384207aad0742709d5687148c16fd95b
@Prakul111 <iamprakul111@gmail.com>
2025-01-02 23:10:29 +0530

    jan-2

--- React/counter/src/App.jsx
+++ React/counter/src/App.jsx
@@ -1,9 +1,18 @@
 import { FunctionComponent } from "./FunctionComponent"
+import { NewComponent } from "./NewComponent"
 
 
 function App() {
 
-  return <FunctionComponent />
+  return (
+    <div>
+
+      < FunctionComponent /> <br /><br />
+      <NewComponent />
+    </div>
+
+  )
+
 }
 
 export default App
