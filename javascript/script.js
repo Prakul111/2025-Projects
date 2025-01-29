ed973019a59d63f3ec9d66dd18ebf8a56eed32b3
@Prakul111 <iamprakul111@gmail.com>
2025-01-29 18:27:07 +0530

    jan 29-01

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,31 @@ const form = document.querySelector('#new-item-form')
 const list = document.querySelector('#list')
 const input = document.querySelector('#item-input')
 
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(input.value);
+
+    // Create an item list 
+    const item = document.createElement('div')
+    item.innerText = input.value
+    item.classList.add('list-item')
+
+    // add that input to the list 
+
+    list.appendChild(item)
+
+    // clear input
+
+    input.value = ""
+
+    // setup event listener to delete item when clicked
+
+    item.addEventListener('click', () => {
+        list.removeChild(item)
+    })
+})
+
+
+
