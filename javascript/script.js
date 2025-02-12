d0fb13c52b1084067ff02c01f1daeb15a3232592
@Prakul111 <iamprakul111@gmail.com>
2025-02-12 11:52:10 +0530

    map-2

---
+++ javascript/script.js
@@ -0,0 +1,50 @@
+// const items = [
+//     {
+//         id: 1,
+//         name: 'test',
+//         description: 'desc'
+//     },
+//     {
+//         id: 2,
+//         name: 'test 2',
+//         description: 'desc'
+//     },
+//     {
+//         id: 3,
+//         name: 'test 3',
+//         description: 'desc'
+//     },
+// ]
+
+
+
+
+
+
+const item = new Map([
+    [1, {
+        id: 1,
+        name: 'test',
+        description: 'desc'
+    }],
+    [2, {
+        id: 1,
+        name: 'test',
+        description: 'desc'
+    }],
+    [3, {
+        id: 1,
+        name: 'test',
+        description: 'desc'
+    }]
+])
+
+
+function getItems(id) {
+    return item.has(id)
+}
+
+console.log(getItems(1));
+
+
+
