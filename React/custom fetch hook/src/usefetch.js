7f378c351426cd47dd67c0245b965b180a2d7645
@Prakul111 <iamprakul111@gmail.com>
2025-02-22 02:04:31 +0530

    hook comp

--- React/custom fetch hook/src/usefetch.js
+++ React/custom fetch hook/src/usefetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export function useFetch(url) {
+export function useFetch(url, options = {}) {
     const [data, setData] = useState()
     const [isError, setIsError] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
@@ -11,10 +11,44 @@ export function useFetch(url) {
         setIsLoading(true)
 
 
-        fetch(url).then(res => res.json())
+        const controller = new AbortController()
+
+        fetch(url, { signal: controller.signal, ...options })
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                }
+                return Promise.reject(res)
+            })
             .then(setData)
-            .catch(() => setIsError(true))
-            .finally(() => setIsLoading(false))
+            .catch((e) => {
+                if (e.name === "AbortError") return
+                setIsError(true)
+            })
+            .finally(() => {
+                if (controller.signal.aborted) return
+                setIsLoading(false)
+            })
+
+        return () => {
+            controller.abort()
+        }
     }, [url])
+
     return { data, isError, isLoading }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
